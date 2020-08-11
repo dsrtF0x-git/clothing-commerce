8cbaf89fc40d5491d18d676ea6fe2315537918f0
Serhii <gespenst.serge@yandex.ru>
2020-08-11 10:08:32 +0300

    added routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
+
+const Hatspage = () => {
+  return <div>HATS PAGE</div>;
+};
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        {/* <Homepage /> */}
+        <Route exact path="/hats" component={Hatspage} />
+      </Switch>
     </div>
   );
 }
