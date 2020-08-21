bab416199e881f5862b3c36dce8646d4bb0d1228
Serhii <gespenst.serge@yandex.ru>
2020-08-21 19:49:11 +0300

    added authentification

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
 import Shop from "./pages/shop/Shop";
 import SignInAndSignUp from "./components/sign-in-sign-up/SignInAndSignUp";
 import Header from "./components/header/Header";
+import { auth } from "./firebase/firebase.utils";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  let unSubscribeFromAuth = null;
+
+  useEffect(() => {
+    unSubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+    });
+
+    return () => {
+      unSubscribeFromAuth();
+    };
+  }, []);
+
   return (
     <div className="App">
       <Header />
