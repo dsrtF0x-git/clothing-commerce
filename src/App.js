725232cb6c567d5e3642f3e65480756d8e18a2e3
Serhii <gespenst.serge@yandex.ru>
2020-09-03 09:20:32 +0300

    added sign in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
 import Shop from "./pages/shop/Shop";
 import { connect } from "react-redux";
@@ -8,6 +8,7 @@ import SignInAndSignUp from "./components/sign-in-sign-up/SignInAndSignUp";
 import Header from "./components/header/Header";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
+import SignIn from "./components/sign-in-sign-up/SignIn";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -28,7 +29,6 @@ class App extends React.Component {
           });
         });
       }
-
       setCurrentUser({ currentUser: userAuth });
     });
   }
@@ -44,15 +44,25 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/shop" component={Shop} />
-          <Route path="/signin" component={SignInAndSignUp} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUp />
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.scss";
 import { ReactComponent as Logo } from "../../images/crown.svg";
+import CartIcon from "../cart-icon/CartIcon";
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
 
 function Header({ currentUser }) {
+  console.log(!!currentUser);
   return (
     <div className="header">
       <Link to="/" className="logo-container">
@@ -19,14 +21,20 @@ function Header({ currentUser }) {
           Contact
         </Link>
         {currentUser ? (
-          <div className="option" onClick={() => auth.signOut()}>
+          <div
+            className="option"
+            onClick={() => {
+              auth.signOut();
+            }}
+          >
             Sign Out
           </div>
         ) : (
-          <Link className="option" to="/signIn">
+          <Link className="option" to="/signin">
             Sign In
           </Link>
         )}
+        <CartIcon />
       </div>
     </div>
   );
