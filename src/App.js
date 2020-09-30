c799530a24e0f0306f417b405e80779fd93027a6
Serhii <gespenst.serge@yandex.ru>
2020-09-30 10:11:23 +0300

    added addCollections method

--- src/App.js
+++ src/App.js
@@ -6,17 +6,22 @@ import Shop from './pages/shop/Shop';
 import { connect } from 'react-redux';
 import SignInAndSignUp from './components/sign-in-sign-up/SignInAndSignUp';
 import Header from './components/header/Header';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import {
+  auth,
+  createUserProfileDocument,
+  addCollectionAndDocuments,
+} from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { createStructuredSelector } from 'reselect';
 import Checkout from './pages/checkout/Checkout';
 import { selectCurrentUser } from './redux/user/user.selectors';
+import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser, collectionsArray } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
@@ -31,6 +36,7 @@ class App extends React.Component {
       }
 
       setCurrentUser(userAuth);
+      addCollectionAndDocuments('collections', collectionsArray);
     });
   }
 
@@ -61,6 +67,7 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
+  collectionsArray: selectCollectionsForPreview,
 });
 
 const mapDispatchToProps = (dispatch) => ({
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,15 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyCji3NmYJcOPBenDM4cUy-2lDITLzteSA8",
-  authDomain: "clothing-commerce.firebaseapp.com",
-  databaseURL: "https://clothing-commerce.firebaseio.com",
-  projectId: "clothing-commerce",
-  storageBucket: "clothing-commerce.appspot.com",
-  messagingSenderId: "398022634966",
-  appId: "1:398022634966:web:938f7ac19b1ce7d9a28877",
+  apiKey: 'AIzaSyCji3NmYJcOPBenDM4cUy-2lDITLzteSA8',
+  authDomain: 'clothing-commerce.firebaseapp.com',
+  databaseURL: 'https://clothing-commerce.firebaseio.com',
+  projectId: 'clothing-commerce',
+  storageBucket: 'clothing-commerce.appspot.com',
+  messagingSenderId: '398022634966',
+  appId: '1:398022634966:web:938f7ac19b1ce7d9a28877',
 };
 
 firebase.initializeApp(config);
@@ -32,18 +32,23 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData,
       });
     } catch (error) {
-      console.log("error creating user", error.message);
+      console.log('error creating user', error.message);
     }
   }
 
   return userRef;
 };
 
+export const addCollectionAndDocuments = (collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+  console.log(collectionRef);
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
