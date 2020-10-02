c92bcc3c20e6cbf8a2ff4ef640ea3158d9c3d02d
Serhii <gespenst.serge@yandex.ru>
2020-10-02 09:33:28 +0300

    sqdsdfsdgsdf

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import Collection from '../collection/Collection';
 import CollectionsOverView from '../../components/collections-overview/CollectionsOverView';
+import {
+  firestore,
+  convertCollectionSnapshotToMap,
+} from '../../firebase/firebase.utils';
 
 function Shop({ match }) {
+  const [unsubscribeFromSnapshot, setUnsubscribeFromSnapshot] = useState(null);
+
+  useEffect(() => {
+    const collectionRef = firestore.collection('collections');
+    collectionRef.onSnapshot(async (snapshot) => {
+      convertCollectionSnapshotToMap(snapshot);
+    });
+  }, []);
+
   return (
     <div className='shop-page'>
       <Route exact path={`${match.path}`} component={CollectionsOverView} />
