647ac25bd6cc4b57e5a9a64bd85ea8a74da1c50b
Serhii <gespenst.serge@yandex.ru>
2020-11-02 08:40:26 +0300

    /payment route c4reated

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import path from 'path';
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 if (process.env.NODE_ENV !== 'production') {
-  import('dotenv').config();
+  require('dotenv').config();
 }
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -26,4 +28,20 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, (error) => {
   if (error) throw error;
   console.log(`Server is running on port ${port}`);
+});
+
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd',
+  };
+
+  stripe.charges.create(body, (error, response) => {
+    if (error) {
+      res.status(500).send({ error });
+    } else {
+      res.status(200).send({ success: response });
+    }
+  });
 });
