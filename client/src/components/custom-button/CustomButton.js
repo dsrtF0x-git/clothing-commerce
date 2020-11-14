317982c3b58fe8affd9a036811a4c568cc8e77e1
Serhii <gespenst.serge@yandex.ru>
2020-11-14 10:16:46 +0300

    graphql added

--- client/src/components/custom-button/CustomButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './CustomButton.scss';
-import { CustomButtonContainer } from './CustomButtonStyles';
-
-function CustomButton({ children, ...props }) {
-  return <CustomButtonContainer {...props}>{children}</CustomButtonContainer>;
-}
-
-export default CustomButton;
