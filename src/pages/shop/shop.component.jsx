2203ce5dd4edea15edb0656ef005c695a3050cc5
Serhii <gespenst.serge@yandex.ru>
2020-11-16 09:53:06 +0300

    added coll container

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import {default as CollectionsOverview} from '../../components/collections-overview/collection-overview-container';
-import CollectionPage from '../collection/collection.component';
+import CollectionsPageContainer from '../collection/collection.container';
 
 const ShopPage = ({ match }) => (
   <div className='shop-page'>
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionsPageContainer} />
   </div>
 );
 
